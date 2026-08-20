fd9bdab3e08c7675c3c2d43836bbbc0d230e528c
MariCSilva <mariana.casilva0633@gmail.com>
2026-08-20 12:12:57 -0300

    aula 04 - confimar a sessão - 20/08/2026

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include 'conexao.php';
+?>
+
 <?php include 'cabecalho.php'; ?>
 
 <?php
@@ -9,10 +14,17 @@
         $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha ='$senha'";
         $resultado = mysqli_query($conexao, $sql);
         if (mysqli_num_rows($resultado) == 1) {
-            $mensagem = "Login realizado com sucesso!";
+            $usuario = mysqli_fetch_assoc($resultado);
+
+            $_SESSION['usuario_id'] = $usuario['id'];
+            $_SESSION['usuario_nome'] = $usuario['nome'];
+
+        header("Location: produtos/listar.php");
+            exit;
         } else {
             $mensagem = "E-mail ou senha inválidos.";
         }
+
     }
 ?>
 
