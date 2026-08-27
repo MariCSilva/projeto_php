46ba872de187e91205eb27c7bed26389535c4d42
mari <mariana.casilva0633@gmail.com>
2026-08-27 12:24:29 -0300

    aula 05 - adição de tabela - 27/08/2026

--- produtos/listar.php
+++ produtos/listar.php
@@ -1,7 +1,42 @@
-<?php require __DIR__ .'/../verifica_login.php'; ?>
-<?php require __DIR__ .'/../cabecalho.php'; ?>
+<?php
+require __DIR__ . '/verifica_login.php';
+require __DIR__ . '/../conexao.php';
+$sql = "SELECT * FROM produtos";
+$resultado = mysqli_query($conexao, $sql);
+?>
+
+
+<?php require __DIR__ . '/../cabecalho.php'; ?>
+
 <main>
-    <p>Bem-vindo(a), <?php echo $_SESSION['usuario_nome']; ?>!</p>
-    <!-- conteúdo da página -->
+    <h2>Produtos cadastrados</h2>
+    <a class="butao" href="cadastrar.php">Cadastrar novo produto</a>
+
+    <div class="tabela">
+        <table>
+            <tr>
+                <th>Produto</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th>Qtd.</th>
+                <th>Ações</th>
+            </tr>
+
+            <?php while ($produto = mysqli_fetch_assoc($resultado)) { ?>
+                <tr>
+                    <td><?php echo $produto['nome']; ?></td>
+                    <td><?php echo $produto['descricao']; ?></td>
+                    <td>R$ <?php echo $produto['preco']; ?></td>
+                    <td><?php echo $produto['quantidade']; ?></td>
+                    <td>
+                        <a class="butaoedit" href="atualizar.php?id=<?php echo $produto['id'];
+                        ?>">Editar</a>
+                        <a class="butaoexc" href="excluir.php?id=<?php echo $produto['id'];
+                        ?>">Excluir</a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
 </main>
-<?php require __DIR__ .'/../rodape.php'; ?>
+<?php require __DIR__ . '/../rodape.php'; ?>
